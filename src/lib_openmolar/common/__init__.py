76e8b43d9ca58afc9a1376ca1d5853b9ae6a2c5a
Neil Wallace <rowinggolfer@googlemail.com>
2012-01-14 20:59:24 +0000

    forgot to include an __init__.py to new package qt4

--- src/lib_openmolar/common/__init__.py
+++ src/lib_openmolar/common/__init__.py
@@ -2,5 +2,5 @@ import settings
 SETTINGS = settings.CommonSettingsInstance()
 
 __all__ = ["settings", "qrc_resources", "classes", "dialogs",
-           "resources", "widgets", "connect", "import_export",
+           "resources", "qt4", "connect", "import_export",
            "db_orm"]
