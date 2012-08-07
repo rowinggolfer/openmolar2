2a2f5d77fcfc4d01aa2087ec2ee465a6b8cdcce9
Neil Wallace <rowinggolfer@googlemail.com>
2012-08-07 11:37:50 +0100

    refactoring of server.

--- src/shell_scripts/install_fuzzymatch.sh
+++ src/shell_scripts/install_fuzzymatch.sh
@@ -13,5 +13,8 @@ echo "## from /usr/share/postgresql/8.4/contrib/fuzzystrmatch.sql               
 echo "##                                                                        ##"
 echo "############################################################################\n"
 
+##TODO - make this use the correct version of the script!!
+##8.4 may not be the db version!
+
 cat /usr/share/postgresql/8.4/contrib/fuzzystrmatch.sql | su postgres -c "psql $DATABASE"
 
